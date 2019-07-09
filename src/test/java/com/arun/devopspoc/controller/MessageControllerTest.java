91297b5d4c6ed5107ad07391e4ff801e73a94796
Arun Ponnusamy <arunvivekp@gmail.com>
2019-07-09 21:52:36 +0000

    Test Class

---
+++ src/test/java/com/arun/devopspoc/controller/MessageControllerTest.java
@@ -0,0 +1,28 @@
+package com.arun.devopspoc.controller;
+
+import junit.framework.TestCase;
+import com.arun.devopspoc.model.Message;
+import com.arun.devopspoc.controller.MessageController;
+import org.junit.Test;
+
+public class MessageControllerTest extends TestCase
+{
+	public Message m = null;
+	public MessageController mc = null;
+
+	protected void setUp() throws Exception {
+		m = new Message();
+		m.setMessage("Arun");
+		mc = new MessageController();
+	}	
+
+	@Test
+	public void testPrintMessage() {
+		assetEquals(mc.printMessage(m), "Message : Arun");
+	}
+
+	protected void tearDown() throws Exception {
+		m = null;
+		mc = null;
+	}
+}
